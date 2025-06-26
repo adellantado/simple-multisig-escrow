cea5f6468e4d53ecdc3e2c8c1ec418de2e0d590e
Alex <adellantado@gmail.com>
2025-06-26 15:50:53 +0300

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter"
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -15,7 +19,29 @@ const config: HardhatUserConfig = {
     Hardhat: {
       chainId: 31337,
       url: "http://127.0.0.1:8545"
-    }
+    },
+    polygon: {
+      url: "https://polygon-rpc.com",
+      accounts: process.env.POLYGON_PRIVATE_KEY ? [process.env.POLYGON_PRIVATE_KEY] : [],
+      chainId: 137,
+    },
+    amoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
+      chainId: 80001,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    L1: "polygon",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    etherscan: process.env.ETHERSCAN_API_KEY,
+    enabled: process.env.GAS_REPORT === "true"
   }
 };
 
