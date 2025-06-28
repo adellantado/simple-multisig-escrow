e4f1da666bb59942c80db0b719d932af32e3da93
Alex <adellantado@gmail.com>
2025-06-28 23:24:09 +0300

    vite .env factory address from global

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
+import { loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [vue()],
-  root: 'src',
-  build: {
-    outDir: '../dist'
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [vue()],
+    root: 'src',
+    build: {
+      outDir: '../dist'
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    optimizeDeps: {
+      include: ['vue', 'vue-router']
+    },
+    define: {
+      // Expose env variables to the client
+      'process.env.VITE_FACTORY_ADDRESS': JSON.stringify(env.VITE_FACTORY_ADDRESS)
     }
-  },
-  optimizeDeps: {
-    include: ['vue', 'vue-router']
   }
 }) 
