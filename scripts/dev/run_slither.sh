d568a66c9cfac388a27a6b272f2badd057a92ed9
Alex <adellantado@gmail.com>
2025-07-14 18:14:49 +0300

    add slither analyzer

--- scripts/dev/run_slither.sh
+++ scripts/dev/run_slither.sh
@@ -5,7 +5,7 @@ green='\033[0;32m' #defining green variable
 blue='\033[0;34m' #defining blue variable
 white='\033[0;37m' #defining blue variable
 
-echo "Running Mythril analysis..."
+echo "Running Slither analysis..."
 echo -e "${blue}Note: Analysis may take a while"
 echo -e "${white}Checking Python version..."
 PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
@@ -19,20 +19,20 @@ fi
 echo -e "${green}Python version is sufficient: $PYTHON_VERSION"
 
 echo -e "${white}Checking if virtual environment exists..."
-if [ ! -d "venv-mythril" ]; then
+if [ ! -d "venv-slither" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv venv-mythril
+    python3 -m venv venv-slither
     echo -e "${green}Virtual environment created successfully."
 fi
 
 echo -e "${white}Activating virtual environment..."
-source venv-mythril/bin/activate
+source venv-slither/bin/activate
 
-echo "Checking if Mythril is installed..."
-if ! which myth >/dev/null 2>&1; then
-    echo "Installing Mythril..."
-    pip install mythril
-    echo -e "${green}Mythril installed successfully."
+echo -e "${white}Checking if Slither is installed..."
+if ! which slither >/dev/null 2>&1; then
+    echo "Installing Slither..."
+    pip install slither-analyzer
+    echo -e "${green}Slither installed successfully."
 fi
 
 echo -e "${white}Flattenering contracts..."
@@ -40,13 +40,13 @@ npx hardhat flatten contracts/MultisigEscrowFactory.sol > MultisigEscrowFactoryF
 npx hardhat flatten contracts/MultisigEscrow.sol > MultisigEscrowFlattened.sol
 
 echo -e "${white}----------------------------------------"
-echo "Running Mythril analysis on MultisigEscrowFactory.sol..."
-myth analyze MultisigEscrowFactoryFlattened.sol --solv 0.8.26
-echo -e "${green}Mythril analysis completed for MultisigEscrowFactory.sol"
+echo -e "${white}Running Slither analysis on MultisigEscrowFactory.sol..."
+slither MultisigEscrowFactoryFlattened.sol
+echo -e "${green}Slither analysis completed for MultisigEscrowFactory.sol"
 echo -e "${white}----------------------------------------"
-echo -e "${white}Running Mythril analysis on MultisigEscrow.sol..."
-myth analyze MultisigEscrowFlattened.sol --solv 0.8.26
-echo -e "${green}Mythril analysis completed for MultisigEscrow.sol"
+echo -e "${white}Running Slither analysis on MultisigEscrow.sol..."
+slither MultisigEscrowFlattened.sol
+echo -e "${green}Slither analysis completed for MultisigEscrow.sol"
 echo -e "${white}----------------------------------------"
 
 echo -e "${white}Removing flattened files..."
@@ -56,4 +56,4 @@ rm -f MultisigEscrowFlattened.sol
 echo "Deactivating virtual environment..."
 deactivate
 
-echo -e "${green}Mythril analysis completed."
+echo -e "${green}Slither analysis completed."
